b9d75e34eb6fb5ba926c67e7ffc57bd856705cf8
caikelun <caikelun@qiyi.com>
2018-01-26 11:53:18 +0800

    Add armeabi-v7a lib to aar. Add upload task in gradle conf.

--- install_libs.sh
+++ install_libs.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
 mkdir -p ./xhookwrapper/xhook/libs/armeabi
+mkdir -p ./xhookwrapper/xhook/libs/armeabi-v7a
 mkdir -p ./xhookwrapper/xhook/libs/arm64-v8a
-cp -fr ./libxhook/libs/armeabi/libxhook.so   ./xhookwrapper/xhook/libs/armeabi/
-cp -fr ./libxhook/libs/arm64-v8a/libxhook.so ./xhookwrapper/xhook/libs/arm64-v8a/
+
+cp -f ./libxhook/libs/armeabi/libxhook.so     ./xhookwrapper/xhook/libs/armeabi/
+cp -f ./libxhook/libs/armeabi-v7a/libxhook.so ./xhookwrapper/xhook/libs/armeabi-v7a/
+cp -f ./libxhook/libs/arm64-v8a/libxhook.so   ./xhookwrapper/xhook/libs/arm64-v8a/
 
 mkdir -p ./xhookwrapper/biz/libs/armeabi
+mkdir -p ./xhookwrapper/biz/libs/armeabi-v7a
 mkdir -p ./xhookwrapper/biz/libs/arm64-v8a
-cp -fr ./libbiz/libs/armeabi/libbiz.so       ./xhookwrapper/biz/libs/armeabi/
-cp -fr ./libbiz/libs/arm64-v8a/libbiz.so     ./xhookwrapper/biz/libs/arm64-v8a/
+
+cp -f ./libbiz/libs/armeabi/libbiz.so         ./xhookwrapper/biz/libs/armeabi/
+cp -f ./libbiz/libs/armeabi-v7a/libbiz.so     ./xhookwrapper/biz/libs/armeabi-v7a/
+cp -f ./libbiz/libs/arm64-v8a/libbiz.so       ./xhookwrapper/biz/libs/arm64-v8a/
 
 mkdir -p ./xhookwrapper/app/libs/armeabi
+mkdir -p ./xhookwrapper/app/libs/armeabi-v7a
 mkdir -p ./xhookwrapper/app/libs/arm64-v8a
-cp -fr ./libtest/libs/armeabi/libtest.so     ./xhookwrapper/app/libs/armeabi/
-cp -fr ./libtest/libs/arm64-v8a/libtest.so   ./xhookwrapper/app/libs/arm64-v8a/
+
+cp -f ./libtest/libs/armeabi/libtest.so       ./xhookwrapper/app/libs/armeabi/
+cp -f ./libtest/libs/armeabi-v7a/libtest.so   ./xhookwrapper/app/libs/armeabi-v7a/
+cp -f ./libtest/libs/arm64-v8a/libtest.so     ./xhookwrapper/app/libs/arm64-v8a/
