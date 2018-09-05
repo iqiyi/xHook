dac3f0f7064e0127c6fcdea721afa5366c6b1456
yinjimmy <jimmy.yin5@gmail.com>
2018-09-05 11:01:09 +0800

    fix: adb shell "chmod +x /data/local/tmp/main" Bad mode

--- docs/overview/code/run.sh
+++ docs/overview/code/run.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#!/bin/bash -e
 
 adb push ./main/libs/armeabi-v7a/libtest.so ./main/libs/armeabi-v7a/main /data/local/tmp
-adb shell "chmod +x /data/local/tmp/main"
+adb shell "chmod 777 /data/local/tmp/main"
 adb shell "export LD_LIBRARY_PATH=/data/local/tmp; /data/local/tmp/main"
